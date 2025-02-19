fef6c41fe379050dabad33ab019d818be02d0d70
Tamarozi <cauaptamarozi@gmail.com>
2025-02-18 23:21:54 -0300

    Add files via upload

---
+++ exe 2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+  int nascimento = 2006;
+  int idade = 2026 - nascimento;
+  printf("nascimento: %d", nascimento);
+  printf("idade: %d", idade);
+  return 0;
+}  
---
+++ exe 4.c
@@ -0,0 +1,37 @@
+#include "stdio.h"
+
+int main() {
+  char letra = 'Y';
+  char A = letra + 1;
+   char B = letra + 2;
+    char C = letra + 3;
+     char D = letra + 4;
+      char E = letra + 5;
+       char F = letra + 6;
+        char G = letra + 7;
+         char H = letra + 8;
+          char I = letra + 9;
+           char J = letra + 10;
+  printf("Letra: %c", letra);
+  printf("\n");
+  printf("Letra: %c", A);
+  printf("\n");
+  printf("Letra: %c", B);
+  printf("\n");
+  printf("Letra: %c", C);
+  printf("\n");
+  printf("Letra: %c", D);
+  printf("\n");
+  printf("Letra: %c", E);
+  printf("\n");
+  printf("Letra: %c", F);
+  printf("\n");
+  printf("Letra: %c", G);
+  printf("\n");
+  printf("Letra: %c", H);
+  printf("\n");
+  printf("Letra: %c", I);
+  printf("\n");
+  printf("Letra: %c", J);
+  return 0;
+}  
