6e66a2032fabe1f2f3b861c3601312dcdaddf6d4
Tamarozi <cauaptamarozi@gmail.com>
2025-02-18 23:25:30 -0300

    Add files via upload

---
+++ exe 3.c
@@ -0,0 +1,13 @@
+#include "stdio.h"
+
+int main() {
+    float usd = 5.685;
+    float eur = 5.939;
+    float btc = 543730.875;
+    printf("Dólar: %.3f", usd);
+    printf("\n");
+    printf("Euro: %.3f", eur);
+    printf("\n");
+    printf("Bitcoin: %.3f", btc);
+    return 0;
+}
